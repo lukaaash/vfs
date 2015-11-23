2861ccef3a6d73eb0c131fdfa16577456bd2e79e
Lukas Pokorny <lukas.pokorny@rebex.net>
2015-11-23 22:05:21 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# vfs
-SFTP over WebSockets support for SSHFS
+vfs
+===
+
+VFS is an experimental wrapper around [SSHFS](http://fuse.sourceforge.net/sshfs.html) written in Node.js.
+It makes it possible to mount remote filesystems accessible through [SFTP over WebSockets](http://sftp.ws/)
+(instead of common SFTP over SSH).
+
+In future, it might support other kinds of virtual filesystems as well.
+
+**Note:** At the client side, Linux with `sshfs` and Node.js is required.
+
+## Getting started
+
+1. Setup an [SFTP over WebSockets server](https://www.npmjs.com/package/sftp-ws/) at the remote machine (or skip this step and try `wss://nuane.com/sftp`).
+2. Install `sshfs` at the client (in Debian/Ubuntu, run `apt-get install sshfs` as root).
+3. Install `vfs` at the client by running `npm install vfs` (add `-g` as root to install globally).
+4. Mount a remote filesystem to a local directory by running `vfs url mountpoint` (for example `vfs wss://nuane.com/sftp ~/sftp).
+5. Enjoy the remote filesystem! :-)
+
+## Missing features
+
+- Add the ability to easily start the filesystem client in the background.
+- Make it possible to pass SSHFS and FUSE options from command line.
+- More authentication options (only `Basic` authentication is supported at the moment, which is insufficient).
---
+++ index.js
@@ -0,0 +1,109 @@
+var spawn = require("child_process").spawn;
+var Url = require("url");
+var shell = require("minish");
+var SFTP = require("sftp-ws");
+
+var StreamChannel = SFTP.Internals.StreamChannel;
+var WebSocketChannelFactory = SFTP.Internals.WebSocketChannelFactory;
+
+// parse arguments with minimist
+var args = shell.parse(process.argv);
+var url = args._[2];
+var path = args._[3];
+
+// handle arguments
+if (args.h) {
+    console.log("usage: vfs url mountpoint [options]");
+    process.exit(1);
+} else if (!url) {
+    console.log("missing url");
+
+    if (!path) {
+	    console.log("see `vfs -h` for usage");
+    }
+
+    process.exit(1);
+} else if (!path) {
+    console.log("missing mountpoint");
+    process.exit(1);
+}
+
+// connect and authenticate
+url = Url.parse(url);
+var factory = new WebSocketChannelFactory();
+var options = { authenticate: authenticate, protocol: "sftp" };
+console.log("connecting to %s", url.host);
+factory.connect(Url.format(url), options, function (err, remote) {
+    if (err) return error(err);
+
+    console.log("connected");
+
+    // spawn `sshfs` in slave mode
+    var child;
+    try {
+	    child = spawn("sshfs", [url.host + ":", path, "-o", "slave"], { stdio: ["pipe", "pipe", process.stderr] });
+    } catch (err) {
+	    return error(err);
+    }
+
+    var local = new StreamChannel(child.stdout);
+
+    var state = 0;
+
+    local.on("message", function (packet) {
+    	//console.log("->", packet);
+	    remote.send(packet);
+    });
+
+    local.on("close", function () {
+	    remote.close();
+    });
+
+    remote.on("message", function (packet) {
+	    //console.log("<-", packet);
+	    child.stdin.write(packet);
+    });
+
+    remote.on("close", function () {
+	    local.close();
+    });
+
+    child.on("exit", function (code, signal) {
+        //console.log("exit");
+	    process.exit(code);
+    });
+
+    console.log("press Ctrl+C to exit");
+});
+
+function error(err) {
+    console.log(err);
+    process.exit(255);
+}
+
+// authenticate the client
+function authenticate(instructions, queries, callback) {
+    if (instructions) shell.write(instructions);
+    
+    var credentials = {};
+    next();
+    
+    function next() {
+        var query = queries.shift();
+        
+        // no more queries -> pass the credentials to the callback
+        if (!query) return callback(credentials);
+        
+        // query the user for credentials
+        if (query.secret) {
+            shell.password(query.prompt, reply);
+        } else {
+            shell.question(query.prompt, reply);
+        }
+        
+        function reply(value) {
+            credentials[query.name] = value;
+            next();
+        }
+    }
+}
